d9e264e12400d84e4ec263c1fa338e8b48c49fa7
supunkamburugamuve <supun06@gmail.com>
2019-12-29 20:56:45 -0500

    adding input fix

--- src/main/java/org/twister2/storage/spark/tera/ByteInputSplit.java
+++ src/main/java/org/twister2/storage/spark/tera/ByteInputSplit.java
@@ -1,11 +1,13 @@
 package org.twister2.storage.spark.tera;
 
+import com.esotericsoftware.kryo.KryoSerializable;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.InputSplit;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 
 public class ByteInputSplit extends InputSplit implements Writable {
   private int elements = 10000000;
@@ -46,15 +48,6 @@ public class ByteInputSplit extends InputSplit implements Writable {
     this.node = node;
   }
 
-  @Override
-  public void write(DataOutput dataOutput) throws IOException {
-  }
-
-  @Override
-  public void readFields(DataInput dataInput) throws IOException {
-
-  }
-
   public void setElements(int elements) {
     this.elements = elements;
   }
@@ -77,5 +70,18 @@ public class ByteInputSplit extends InputSplit implements Writable {
 
   public int getDataSize() {
     return dataSize;
+  }
+
+  @Override
+  public void write(DataOutput dataOutput) throws IOException {
+    byte[] b = ByteBuffer.allocate(12).putInt(elements).putInt(keySize).putInt(dataSize).array();
+    dataOutput.write(b);
+  }
+
+  @Override
+  public void readFields(DataInput dataInput) throws IOException {
+    elements = dataInput.readInt();
+    keySize = dataInput.readInt();
+    dataSize = dataInput.readInt();
   }
 }
